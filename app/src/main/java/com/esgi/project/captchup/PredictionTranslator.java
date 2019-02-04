782cfa7c0b391dc0425744e458ad11df6ea3c9d1
Célia Casagrande <celia@localhost.localdomain>
2019-02-04 16:33:56 +0100

    translation is working

---
+++ app/src/main/java/com/esgi/project/captchup/PredictionTranslator.java
@@ -0,0 +1,76 @@
+package com.esgi.project.captchup;
+
+
+import android.os.AsyncTask;
+
+import com.esgi.project.captchup.Models.Prediction;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class Translator extends AsyncTask<String, Prediction, Prediction> {
+
+    static String translate(String sourceLanguageCode, String targetLanguageCode, String sourceText) {
+        StringBuilder result = new StringBuilder();
+        try {
+            String encodedText = URLEncoder.encode(sourceText, "UTF-8");
+            String urlStr = "https://www.googleapis.com/language/translate/v2?key=" + "AIzaSyDgZc15rtLGH-UPZ7w3LQPJlL1zd5KyBtU" + "&q=" + encodedText + "&target=" + targetLanguageCode + "&source=" + sourceLanguageCode;
+
+            URL url = new URL(urlStr);
+
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            InputStream stream;
+            if (conn.getResponseCode() == 200) //success
+            {
+                stream = conn.getInputStream();
+            } else
+                stream = conn.getErrorStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+
+            JsonParser parser = new JsonParser();
+
+            JsonElement element = parser.parse(result.toString());
+
+            if (element.isJsonObject()) {
+                JsonObject obj = element.getAsJsonObject();
+                if (obj.get("error") == null) {
+                    String translatedText = obj.get("data").getAsJsonObject().
+                            get("translations").getAsJsonArray().
+                            get(0).getAsJsonObject().
+                            get("translatedText").getAsString();
+                    return translatedText;
+
+                }
+            }
+
+            if (conn.getResponseCode() != 200) {
+                System.err.println(result);
+            }
+
+        } catch (IOException | JsonSyntaxException ex) {
+            System.err.println(ex.getMessage());
+        }
+
+        return null;
+    }
+
+    @Override
+    protected Prediction doInBackground(String... strings) {
+        return null;
+    }
+}
---
+++ app/src/test/java/com/esgi/project/captchup/PredictionUnitTest.java
@@ -0,0 +1,4 @@
+package com.esgi.project.captchup;
+
+public class PredictionUnitTest {
+}
